4d1a08f4ffb7309edbd7eeafb5aec631c1acfc6a
Hanna Borodina <venera131916@gmail.com>
2023-11-14 22:59:50 +0200

    add updates to gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-import * as basicL
 
+const container = document.querySelector(".gallery");
 
-const instance = bas
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+container.addEventListener("click", handleClick);
 
-console.log(galleryItems);
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </li>
+    `
+    )
+    .join("");
+}
+
+function handleClick(event) {
+    
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imgSource = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `<div class="modal">
+<img src="${imgSource}">
+</div>
+`,
+{
+    onShow: (instance) => 
+    {
+        document.addEventListener("keydown", function (event) 
+        {
+            if (event.keyCode == 27 && instance.visible())
+             {
+                instance.close(); 
+            }
+        }
+        );
+    },
+}
+  );
+  instance.show();
+}
